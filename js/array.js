bdbed04f974a1b2d94e8b8ecd18512e251517389
CrazyMKing <CrazyMKing@CrazyMKing-2.local>
2019-02-20 21:29:17 -0500

    LAB

---
+++ js/array.js
@@ -0,0 +1,20 @@
+var Animal["Lion","Penguin","Monkey","Elephant","Shark","Spider"];
+
+var fun1=function()
+{
+  window.alert("rrr")
+  /*var x = document.createElement("OL");
+  x.setAttribute("id", "ol");
+  document.body.appendChild(x);
+  Animal.forEach(function(d)));*/
+}
+/*var y = document.createElement("LI");
+var t = document.createTextNode(d);
+y.appendChild(t);
+document.getElementById("myOl").appendChild(y);*/
+/*Animal.forEach(function(d){
+	var list = document.createElement("LI");
+	var text = document.createTextNode(d);
+	list.appendChild(text);
+	document.getElementById("list").appendChild(list);
+})*/
